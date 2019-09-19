0ea7261c2604821a6a819c0c773238c0eb7c9ed7
nuclearWintersru <armandonarcizoruedaperez@gmail.com>
2019-09-19 09:31:57 -0500

    changessss

--- src/components/componentsA.tsx
+++ src/components/componentsA.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-//import styles from './componentsA.css'
 import styles from './componentA.module.css';
 
 export default class componentsA extends Component {
